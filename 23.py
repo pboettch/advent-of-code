e2703513a8b7704adf6f724ca9cbeba22a902473
Patrick Boettcher <p@yai.se>
2021-01-01 16:14:13 +0100

    23 1st part with lists

--- 23.py
+++ 23.py
@@ -1,34 +1,108 @@
 #!/usr/bin/env python3
 
-input = [int(i) for i in '389125467']
-input_len = len(input)
 
-for move in range(0, 11):
-    print(move, input)
+class CupInCircle:
+    def __init__(self, v: int, p, n):
+        self.val = v
+        self.prev = p
+        self.next = n
 
-    current = move % input_len
-    three_cups = (input + input)[current + 1:current + 4]
-    insert = input[current] - 1
+    def to_str(self, count=-1):
+        s = ''
+        n = self
+        while True:
+            s += str(n.val)
 
-    for cup in three_cups:
-        input.remove(cup)
+            n = n.next
+            if n == self:
+                break
 
+            count -= 1
+            if count == 0:
+                break
+        return s
+
+    def __str__(self):
+        return self.to_str()
+
+    def find(self, val):
+        n = self
+        while True:
+            if n.val == val:
+                return n
+            n = n.next
+            if n == self:
+                break
+        return None
+
+    def __len__(self):
+        l = 0
+
+        n = self
+        while True:
+            l += 1
+            n = n.next
+            if n == self:
+                break
+
+        return l
+
+
+prev = None
+first = None  # start
+for i in '739862541':
+# for i in '389125467':
+    node = CupInCircle(int(i), prev, None)
+
+    if prev is not None:
+        prev.next = node
+
+    if first is None:
+        first = node
+
+    prev = node
+
+# close the circle
+first.prev = prev
+prev.next = first
+
+input_len = len(first)
+print('len', input_len)
+print(first)
+
+current = first
+for move in range(100):
+    print('--', move + 1, '--', len(first))
+    print(first)
+
+    # take out three cups
+    three = current.next
+
+    current.next = three.next.next.next
+    three.next.next.next.prev = current
+
+    print('pick up ', three.to_str(3))
+
+    dest_val = current.val - 1
 
     while True:
-        try:
-            index = input.index(insert)
-        except ValueError:
-            insert -= 1
-            if insert < 0:
-                insert = input_len
-            continue
+        dest = current.find(dest_val)
+        if dest is not None:
+            break
+        else:
+            dest_val -= 1
+            if dest_val < 0:
+                dest_val = input_len
 
-        print('destination', input[index])
-        input.insert(index + 1, three_cups[2])
-        input.insert(index + 1, three_cups[1])
-        input.insert(index + 1, three_cups[0])
-        break
+    print('dest', dest.val)
 
-    current += 1
-    if current > len(input):
-        current = 0
+    dest.next.prev = three.next.next
+    three.next.next.next = dest.next
+
+    dest.next = three
+    three.prev = dest
+
+    current = current.next
+
+one = first.find(1)
+print('part1', one.to_str()[1:])
