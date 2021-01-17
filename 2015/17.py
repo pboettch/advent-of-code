6379cd086aeda871bb9d33c4007ea4e73affaf26
Patrick Boettcher <p@yai.se>
2021-01-17 12:27:18 +0100

    2015-17 with sorting

--- 2015/17.py
+++ 2015/17.py
@@ -10,6 +10,8 @@ def f(containers: List[int], target: int, current: int = 0, cont_count: int = 0)
         global count_combinations
         count_combinations.setdefault(cont_count, 0)
         count_combinations[cont_count] += 1
+        print('current solutions:', sum(count_combinations.values()),
+                                    min(count_combinations.keys()))
         return 1
     elif current > target:
         return 0
@@ -23,10 +25,10 @@ def f(containers: List[int], target: int, current: int = 0, cont_count: int = 0)
 assert f([20, 15, 10, 5, 5], 25) == 4
 assert count_combinations[2] == 3
 
-containers = [int(i) for i in open('17.input').read(10000).strip().split('\n')]
+containers = list(reversed(sorted([int(i) for i in open('super17.input').read(10000).strip().split('\n')])))
 
 count_combinations = {}
-part1 = f(containers, 150)
+part1 = f(containers, 400)
 
 print('part1', part1)
 
