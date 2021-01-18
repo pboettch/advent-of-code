22f5c8653df1b86c9b6ce364f2af6db346b7d67f
Patrick Boettcher <p@yai.se>
2021-01-18 08:42:09 +0100

    add template-script

---
+++ template.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+if __name__ == "__main__":
+    part1 = 0
+    part2 = 0
+
+    for l in open(''):
+        l = l.strip()
+
+    print('part1', part1)
+    print('part2', part2)
