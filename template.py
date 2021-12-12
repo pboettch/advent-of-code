8d0a8125a375bf360920324b54b3fbcd9e6fb2ce
Patrick Boettcher <p@yai.se>
2021-12-12 17:40:19 +0100

    update template to be able to pass another file

--- template.py
+++ template.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import sys
+
 
 if __name__ == "__main__":
     part1 = 0
     part2 = 0
 
-    for l in open(''):
+    for l in open(sys.argv[1] if len(sys.argv) > 1 else '11-input'):
         l = l.strip()
 
     print('part1', part1)
