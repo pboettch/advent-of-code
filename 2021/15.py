dc3a25f55fe54f8636708fbdcac421b8327dd80f
Patrick Boettcher <p@yai.se>
2021-12-15 21:45:52 +0100

    2021-15 (cache)

--- 2021/15.py
+++ 2021/15.py
@@ -1,66 +1,51 @@
 #!/usr/bin/env python3
 
 import sys
-sys.setrecursionlimit(15000)
+
+import numpy as np
+
+
+def risk(G):
+    result = 0
+    C = {(0, 0): 0}
+    end = (len(G[0]) - 1, len(G) - 1)
+    while True:
+        for x in range(len(G[0])):
+            for y in range(len(G)):
+                for n in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                    if n[0] < 0 or n[0] >= len(G[0]):
+                        continue
+                    if n[1] < 0 or n[1] >= len(G):
+                        continue
+
+                    p = (x, y)
+                    r = C[p] + G[n[1]][n[0]]
+
+                    if n not in C or C[n] > r:
+                        C[n] = r
+
+        if result == C[end]:
+            break
+
+        result = C[end]
+    return result
+
+
 if __name__ == "__main__":
-    G = []
-    V = []
+
+    cave = []
 
     for l in open(sys.argv[1] if len(sys.argv) > 1 else '15-input'):
-        G += [[*map(int, l.strip())]]
-        V += [[False] * len(G[-1])]
+        cave += [[*map(int, l.strip())]]
 
+    part1 = risk(cave)
 
+    cave = np.array(cave)
+    cave = np.hstack([cave, cave + 1, cave + 2, cave + 3, cave + 4])
+    cave = np.vstack([cave, cave + 1, cave + 2, cave + 3, cave + 4])
+    cave[cave > 9] -= 9
 
-    for y in range(len(G)):
-        for x in range(len(G[0])):
-            if G[y][x] > 6:
-                V[y][x] = True
-
-
-
-
-    current_sum = 1275
-
-    def find_path(x, y, P):
-        global current_sum
-        if current_sum is not None and P >= current_sum:
-            return
-
-        if x == len(G[0]) - 1 and y == len(G) - 1:
-            if current_sum is None or P < current_sum:
-                print("new min:", P, current_sum)
-                current_sum = P
-            return
-
-        for i in range(4):
-            dx = x
-            dy = y
-            if i == 0:
-                dx = x + 1
-            elif i == 1:
-                dx = x - 1
-            elif i == 2:
-                dy = y + 1
-            elif i == 3:
-                dy = y - 1
-
-            if dx < 0 or dx >= len(G[0]):
-                continue
-            if dy < 0 or dy >= len(G):
-                continue
-            if V[dy][dx]:
-                continue
-
-            V[dy][dx] = True
-            find_path(dx, dy, P + G[dy][dx])
-            V[dy][dx] = False
-
-
-    find_path(0, 0, 0)
-
-    part1 = current_sum
-    part2 = 0
+    part2 = risk(cave.tolist())
 
     print('part1', part1)
     print('part2', part2)
